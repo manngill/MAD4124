c018c13a8d1bfdbdb7bdbf607dd13a34dc9a32c7
Manpreet Kaur <manpreetgill1946@gmail.com>
2018-11-30 08:41:24 -0500

    Verify button action

--- MAD4124_PROJECT/app/src/main/java/com/mad4124/mad4124project/LoginActivity.java
+++ MAD4124_PROJECT/app/src/main/java/com/mad4124/mad4124project/LoginActivity.java
@@ -1,7 +1,9 @@
 package com.mad4124.mad4124project;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -9,5 +11,10 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+    }
+
+    public void login(View v)
+    {
+        startActivity(new Intent(getApplicationContext(), HomeActivity.class));
     }
 }
